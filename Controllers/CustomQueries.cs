a9d71e62621e53759d6eb79e478894b65cdbba8f
Thalía Zapata <thaliazapata@Thalias-MacBook-Pro.local>
2020-11-14 19:01:53 -0500

    getUsuario

--- Controllers/CustomQueries.cs
+++ Controllers/CustomQueries.cs
@@ -165,7 +165,20 @@ namespace Proyecto.Controllers
             var data = consulta;
             return Ok(data);
         }
-
+        [HttpGet("getUsuario/{email}/{pass}")]
+        public IActionResult getBuscar(string email, string pass ,[FromQuery] PaginationFilter filter)
+        {
+            var route = Request.Path.Value;
+            var validFilter = new PaginationFilter(filter.PageNumber, filter.PageSize);
+            var data =  _context.Usuario
+            .FromSqlRaw("select * FROM usuario WHERE useremail = '"+email+"' and userpass = '"+pass+"'")
+            .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
+            .Take(validFilter.PageSize)
+            .ToList();
+            var totalRecords = _context.Usuario.Count();
+            var pagedReponse = PaginationHelper.CreatePagedReponse<Usuario>(data, validFilter, totalRecords, uriService, route);
+            return Ok(pagedReponse);
+        }
 
 
     }
